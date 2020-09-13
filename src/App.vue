b5e348ce932730f30f37573b2936eb1310db2baf
KumaStark <elastic48@msn.com>
2020-09-13 22:02:19 +0800

    控制面板完成

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,176 @@
 <template>
-  <div class="wrapper">
-    <div id="settingPanel">123</div>
-    <h1 id="mytime">{{str}}</h1>
-    <!-- <input id="mytime" type="text" name="" value="显示时间"> -->
-    <div id="controlPanel">
-      <button id="stop" name="button" @click="stop">暂停</button>
-      <button id="start" name="button" @click="start">开始</button>
-      <button id="reset" name="button" @click="reset">重置</button>
+  <div id="counterRoot" @click="checkClickLocation('background')">
+    <div class="wrapper">
+      <div id="settingButton">
+        <el-button
+          type="danger"
+          size="mini"
+          icon="el-icon-setting"
+          @click="checkClickLocation('settingBtn')"
+          :disabled="is_timer_started"
+        ></el-button>
+      </div>
+      <div id="settingPanel" @click="checkClickLocation('settingPanel')">
+        <transition name="el-fade-in-linear">
+          <div v-show="showSettings" class="transition-box">
+            <el-row :gutter="10">
+              <el-col :span="8">
+                <span>小时</span>
+              </el-col>
+              <el-col :span="8">
+                <span>分钟</span>
+              </el-col>
+              <el-col :span="8">
+                <span>秒数</span>
+              </el-col>
+            </el-row>
+            <el-row :gutter="10">
+              <el-col :span="8">
+                <el-input-number v-model="default_h" @change="setDefault" controls-position="right" :min="0" :max="99"></el-input-number>
+              </el-col>
+              <el-col :span="8">
+                <el-input-number v-model="default_m" @change="setDefault" controls-position="right" :min="0" :max="59"></el-input-number>
+              </el-col>
+              <el-col :span="8">
+                <el-input-number v-model="default_s" @change="setDefault" controls-position="right" :min="0" :max="59"></el-input-number>
+              </el-col>
+            </el-row>
+            <el-row :gutter="10" justify="space-between">
+              <el-col :span="12">
+                <el-button
+                  class="preset-button"
+                  type="danger"
+                  icon="el-icon-data-board"
+                  @click="preset1"
+                >述职论文</el-button>
+              </el-col>
+              <el-col :span="12">
+                <el-button
+                  class="preset-button"
+                  type="danger"
+                  icon="el-icon-s-comment"
+                  @click="preset2"
+                >抽题答辩</el-button>
+              </el-col>
+            </el-row>
+          </div>
+        </transition>
+      </div>
+      <h1 id="counterString">{{ time_str }}</h1>
+      <div id="controlPanel">
+        <el-button-group>
+          <el-button
+            type="danger"
+            round
+            @click="pause"
+            icon="el-icon-video-pause"
+            :disabled="sw_pause"
+          >暂停</el-button>
+          <el-button
+            type="danger"
+            round
+            @click="start"
+            icon="el-icon-video-play"
+            :disabled="sw_start"
+          >开始</el-button>
+          <el-button
+            type="danger"
+            round
+            @click="reset"
+            icon="el-icon-refresh"
+            :disabled="sw_reset"
+          >重置</el-button>
+        </el-button-group>
+      </div>
     </div>
   </div>
 </template>
 <script>
+document.title = "计时器";
 export default {
   data() {
     return {
+      showSettings: false,
       default_h: 0,
-      default_m: 8,
+      default_m: 10,
       default_s: 0,
       default_ms: 0,
       h: 0,
       m: 0,
       s: 0,
       ms: 0,
-      time: "Timer",
+      timer_container: "TimerContainer",
+      sw_start: false,
+      sw_pause: true,
+      sw_reset: false,
+      is_timer_started: false,
+      timeup_notifier: new Audio("./notify.wav"),
     };
   },
   computed: {
-    str() {
-      let nowTime =
-        this.toDub(this.h) +
-        ":" +
-        this.toDub(this.m) +
-        ":" +
-        this.toDub(this.s) +
-        ""; /*+this.toDubms(this.ms)+"毫秒"*/
-      return nowTime;
+    time_str() {
+      let check_time_remain = this.h + this.m + this.s + this.ms;
+      if (check_time_remain > 0 && this.h > 0) {
+        return (
+          this.toDub(this.h) +
+          ":" +
+          this.toDub(this.m) +
+          ":" +
+          this.toDub(this.s)
+        );
+      } else if (check_time_remain > 0) {
+        return (
+          this.toDub(this.m) +
+          ":" +
+          this.toDub(this.s) +
+          "." +
+          this.toDub(this.ms)
+        );
+        // this.toDubms(this.ms);
+      } else {
+        this.pause();
+        this.timeup_notifier.play();
+        return "计时结束";
+      }
+    },
+  },
+  watch: {
+    is_timer_started: {
+      handler(newValue) {
+        this.sw_pause = !newValue;
+        this.sw_start = newValue;
+        this.showSettings = false;
+      },
+      immediate: true,
     },
   },
   methods: {
-    check() {
-      // 检查倒计时是是否结束
+    preset1() {
+      this.default_h = 0;
+      this.default_m = 10;
+      this.default_s = 0;
+      this.default_ms = 0;
+      this.setDefault();
+      this.showSettings = false;
+      this.start();
+    },
+    preset2() {
+      this.default_h = 0;
+      this.default_m = 15;
+      this.default_s = 0;
+      this.default_ms = 0;
+      this.setDefault();
+      this.showSettings = false;
+      this.start();
+    },
+    checkClickLocation(clickFrom) {
+      event.stopPropagation(); // 阻止点击穿透
+      if (clickFrom == "settingBtn") {
+        this.showSettings = !this.showSettings;
+      } else if (clickFrom == "background") {
+        this.showSettings = false;
+      }
+      // console.log("clickFrom", clickFrom);
     },
     setDefault() {
       this.h = this.default_h;
@@ -49,9 +180,9 @@ export default {
     },
     timer() {
       // 定义计时函数
-      this.ms = this.ms - 50; //毫秒
+      this.ms = this.ms - 1; //毫秒
       if (this.ms < 0) {
-        this.ms = 950;
+        this.ms = 95;
         this.s = this.s - 1; //秒
       }
       if (this.s < 0) {
@@ -63,23 +194,22 @@ export default {
         this.h = this.h - 1; //小时
       }
     },
-
     reset() {
       //重置
-      clearInterval(this.time);
+      this.is_timer_started = false;
+      clearInterval(this.timer_container);
       this.setDefault();
     },
-
     start() {
       //开始
-      this.time = setInterval(this.timer, 50);
+      this.is_timer_started = true;
+      this.timer_container = setInterval(this.timer, 10);
     },
-
-    stop() {
+    pause() {
       //暂停
-      clearInterval(this.time);
+      this.is_timer_started = false;
+      clearInterval(this.timer_container);
     },
-
     toDub(n) {
       //补0操作
       if (n < 10) {
@@ -88,33 +218,46 @@ export default {
         return "" + n;
       }
     },
-
     toDubms(n) {
       //给毫秒补0操作
       if (n < 10) {
         return "00" + n;
-      } else {
+      } else if (n < 100) {
         return "0" + n;
+      } else {
+        return n;
       }
     },
   },
-  // 实例创建完成后调用，此阶段完成了数据的观测等，但尚未挂载，$el 还不可用。需要初始化处理一些数据时会比较有用
   created: function () {
-    console.log("Init Timer Settings ...");
     this.setDefault();
-    console.log("Finished!");
   },
 };
 </script>
 <style>
-#settingPanel {
-  background-color: #0962e9;
+#counterRoot {
   position: fixed;
-  top:20px;
-  left:20px;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+}
+#settingButton {
+  position: fixed;
+  top: 20px;
+  left: 20px;
 }
 
-#mytime {
+#settingPanel {
+  position: fixed;
+  display: flex;
+  margin-top: 20px;
+  height: 100px;
+  top: 20px;
+  left: 60px;
+}
+
+#counterString {
   background: red;
   color: yellow;
   display: block;
@@ -129,29 +272,33 @@ export default {
 body {
   background-color: red;
 }
-button {
-  width: 90px;
-  padding: 0 20px;
-  background-color: #2980ff;
-  border: none;
-  height: 34px;
-  border-color: #2980ff;
+.el-input-number {
+  width: 100px !important;
+}
+.preset-button {
+  width: 100%;
+}
+.transition-box {
+  margin-bottom: 10px;
+  width: 360px;
+  height: 160px;
+  border-radius: 4px;
+  background-color: #409eff;
+  text-align: center;
   color: #fff;
-  cursor: pointer;
-  line-height: 34px;
-  outline: none;
-  -webkit-border-radius: 15px;
-  -moz-border-radius: 15px;
-  /* Old Firefox */
-  border-radius: 15px;
-  -webkit-transition: all 0.15s ease;
-  -moz-transition: all 0.15s ease;
-  -o-transition: all 0.15s ease;
-  -ms-transition: all 0.15s ease;
-  transition: all 0.15s ease;
+  padding: 20px;
+  box-sizing: border-box;
   margin-right: 20px;
 }
-button:hover {
-  background-color: #0962e9;
+
+.el-row {
+  margin-bottom: 10px;
+}
+:last-child {
+  margin-bottom: 0;
+}
+
+.el-col {
+  border-radius: 4px;
 }
 </style>
